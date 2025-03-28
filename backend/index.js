804dbb1622167a80aaa83a271d6fc5b92a270a6e
Michael DeCuire <michaeldecuire18@gmail.com>
2025-03-27 21:07:42 -0500

    Initial mongoDB connection with .env file imported separately.

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import { nanoid } from "nanoid";
+import { config } from "dotenv";
+
+const app = express();
+app.use(express.json());
+
+config();
+
+const uri = process.env.MONGO_URI;
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+    try {
+      // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+      await mongoose.connect(uri, clientOptions);
+      await mongoose.connection.db.admin().command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await mongoose.disconnect();
+    }
+  }
+  run().catch(console.dir);
